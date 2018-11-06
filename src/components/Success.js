d400a7787f9cf6bbd860572cd3510e624b7c7cca
Paul Tobin <tobster111@gmail.com>
2018-11-06 10:50:49 +0000

    MultiViewForm with React + Material UI

---
+++ src/components/Success.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import { List, ListItem } from 'material-ui/List';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class Success extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Success" />
+          <h1>Thank you for Your Submission</h1>
+          <p>Check your email for further instructions</p>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Success;
